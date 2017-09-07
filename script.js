d016f5847360049d18726abdb0a1f2213a62e6f3
danielalb <31521026+danielalb@users.noreply.github.com>
2017-09-07 17:25:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log('hello world!!!!')
-document.write('<h1> Hi!</h1>')
+let x=prompt("Enter an adjective")
+
+let y=prompt("Enter a noun")
+
+let z=prompt("Enter a proper noun") 
+
+adj_list=["interesting", "lazy", "round", x]
+
+noun_list=["hamburger", "giraffes", "computer", y]
+
+proper_noun_list=["WalMart", "Jeff from the Wiggles", "Steve Urkel", z]
+
+"The " + noun_list[Math.floor(Math.random()*noun_list.length)]+ " Not Taken by " 
+  +proper_noun_list[Math.floor(Math.random()*proper_noun_list.length)]+ ": Two " 
+  +noun_list[Math.floor(Math.random()*noun_list.length)] + " diverged in a " 
+  +adj_list[Math.floor(Math.random()*adj_list.length)] + " " + noun_list[Math.floor(Math.random()*noun_list.length)] + 
+  " , and " + proper_noun_list[Math.floor(Math.random()*proper_noun_list.length)] + " - " 
+  + proper_noun_list[Math.floor(Math.random()*proper_noun_list.length)] 
+  + " took the one less traveled by, and that has made all the difference."
